83f7ceaf8e8c46453a14bac93a40125b414bd190
Vladimir Grinenko <i@tadatuta.com>
2015-08-28 18:26:29 +0300

    Do not fail when abbreviation is inside arbitrary text

--- bemmet.js
+++ bemmet.js
@@ -6,8 +6,13 @@ stdin(function (textBeforeCaret) {
     var textBeforeCaretNoSpaces = textBeforeCaret.replace(/\{(.*)\}/g, function(str) {
             return str.replace(/\ /g, 'S'); // S is used as a space placeholder
         }),
-        spaceNearCaretIdx = textBeforeCaretNoSpaces.lastIndexOf(' '),
-        selection = (spaceNearCaretIdx > -1 ? textBeforeCaret.substr(spaceNearCaretIdx) : textBeforeCaret).trim(),
+        spaceNearCaretIdx = textBeforeCaretNoSpaces.lastIndexOf(' ');
+
+    if (spaceNearCaretIdx < 0) {
+        spaceNearCaretIdx = textBeforeCaretNoSpaces.lastIndexOf('\n');
+    }
+
+    var selection = (spaceNearCaretIdx > -1 ? textBeforeCaret.substr(spaceNearCaretIdx) : textBeforeCaret).trim(),
         // regexp gets 'b1' from strings like { block: 'b1' }
         parentBlock = /block['"\s]*:(?:\s)?['"]{1}(.*?)['"]{1}/
             .exec(textBeforeCaretNoSpaces.substr(textBeforeCaretNoSpaces.lastIndexOf('block')));
