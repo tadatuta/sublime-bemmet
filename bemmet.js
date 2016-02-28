0ff9b2c9fa8de034c067418b04f0121eb1a75be9
Igor Adamenko <mail@igoradamenko.com>
2016-02-29 01:03:12 +0300

    move changes from atom-bemmet

--- bemmet.js
+++ bemmet.js
@@ -6,18 +6,14 @@ stdin(function (textBeforeCaret) {
     var textBeforeCaretNoSpaces = textBeforeCaret.replace(/\{(.*)\}/g, function(str) {
             return str.replace(/\ /g, 'S'); // S is used as a space placeholder
         }),
-        spaceNearCaretIdx = textBeforeCaretNoSpaces.lastIndexOf(' ');
-
-    if (spaceNearCaretIdx < 0) {
-        spaceNearCaretIdx = textBeforeCaretNoSpaces.lastIndexOf('\n');
-    }
-
-    var selection = (spaceNearCaretIdx > -1 ? textBeforeCaret.substr(spaceNearCaretIdx) : textBeforeCaret).trim(),
+        lastSpaceSymbol = /\s/.exec(textBeforeCaretNoSpaces.split('').reverse().join('')),
+        spaceNearCaretIdx = lastSpaceSymbol && textBeforeCaretNoSpaces.lastIndexOf(lastSpaceSymbol[0]),
+        selection = ((typeof spaceNearCaretIdx !== null && spaceNearCaretIdx > -1) ? textBeforeCaret.substr(spaceNearCaretIdx) : textBeforeCaret).trim(),
         // regexp gets 'b1' from strings like { block: 'b1' }
         parentBlock = /block['"\s]*:(?:\s)?['"]{1}(.*?)['"]{1}/
             .exec(textBeforeCaretNoSpaces.substr(textBeforeCaretNoSpaces.lastIndexOf('block')));
 
-        parentBlock && parentBlock[1] && (parentBlock = parentBlock[1]);
+    parentBlock && parentBlock[1] && (parentBlock = parentBlock[1]);
 
     try {
         process.stdout.write(textBeforeCaret.replace(selection, bemmet.stringify(selection, parentBlock)));
